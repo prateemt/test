e4e554987ddfc3acd0c7232745fd0a8d51518a12
Rakesh Kanchalwar <rakesh.kanchalwar@bitwiseglobal.com>
2016-06-16 02:23:44 +0530

    updated

--- src/com/mycompany/FilterWithTables.java
+++ src/com/mycompany/FilterWithTables.java
@@ -1,7 +1,10 @@
-package hydrograph.ui.dataviewer.window;
+package com.bitwise.app.graph.propertywindow;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -14,8 +17,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 
 public class FilterWithTables extends Dialog {
 	private TabItem remoteTabItem;
@@ -39,11 +40,19 @@ public class FilterWithTables extends Dialog {
 	private Composite localButtonComposite;
 	private Composite localTableContainerComposite;
 	private Composite localTabFolderComposite;
+	private CellEditor[] editors;
 	
 	public static void main(String[] args) {
 		FilterWithTables test = new FilterWithTables(Display.getDefault().getActiveShell());
 		test.open();
 	}
+	
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		// TODO Auto-generated method stub
+		//super.createButtonsForButtonBar(parent);
+	}
+	
 	/**
 	 * Create the dialog.
 	 * @param parentShell
@@ -88,9 +97,8 @@ public class FilterWithTables extends Dialog {
 	}
 	
 	private void createTabItem(TabFolder tabFolder, TabItem tabItem, Table table, TableViewer tableViewer, 
-			String tabName, Button okButton, Button cancelButton, 
-			Composite tabFolderComposite, Composite tableContainerComposite, Composite buttonComposite, 
-			GridData gdTableComposite, GridData gdButtonComposite) {
+			String tabName, Button okButton, Button cancelButton, Composite tabFolderComposite,
+			Composite tableContainerComposite, Composite buttonComposite, GridData gdTableComposite, GridData gdButtonComposite) {
 		
 		tabFolderComposite = new Composite(tabFolder, SWT.NONE);
 		tabItem.setControl(tabFolderComposite);
@@ -98,43 +106,39 @@ public class FilterWithTables extends Dialog {
 		
 		tableContainerComposite = new Composite(tabFolderComposite, SWT.NONE);
 		tableContainerComposite.setLayout(new GridLayout(1, false));
-		gdTableComposite = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1);
-		gdTableComposite.heightHint = 244;
+		gdTableComposite = new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1);
+		gdTableComposite.heightHint = 232;
 		gdTableComposite.widthHint = 640;
 		tableContainerComposite.setLayoutData(gdTableComposite);
 		
-		tableViewer = new TableViewer(tableContainerComposite, SWT.BORDER | SWT.FULL_SELECTION);
+		tableViewer = new TableViewer(tableContainerComposite, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
 		table = tableViewer.getTable();
+		table.setHeaderVisible(true);
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
-		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn.setWidth(100);
-		tblclmnNewColumn.setText("New Column");
-		
-		TableItem tableItem = new TableItem(table, SWT.NONE);
-		tableItem.setText("New TableItem");
 		new Label(tabFolderComposite, SWT.NONE);
 		
 		buttonComposite = new Composite(tabFolderComposite, SWT.NONE);
 		buttonComposite.setLayout(new GridLayout(2, false));
-		gdButtonComposite = new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1);
+		gdButtonComposite = new GridData(SWT.RIGHT, SWT.FILL, true, true, 1, 1);
 		gdButtonComposite.heightHint = 31;
 		buttonComposite.setLayoutData(gdButtonComposite);
 		
 		
-		okButton = new Button(buttonComposite, SWT.NONE);
-		okButton.setText("Ok");
-		
-		cancelButton = new Button(buttonComposite, SWT.NONE);
-		cancelButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		cancelButton.setText("Cancel");
-		new Label(tabFolderComposite, SWT.NONE);
+		getButton(okButton, "Ok", buttonComposite);
+		getButton(cancelButton, "Cancel", buttonComposite);
 	}
-}
 
-class FilterConditions{
-	private String fieldName;
-	private String relationalOperator;
-	private String conditionalOperator;
-	private String value;
+	private void getButton(Button button, String buttonName, Composite buttonComposite){
+		button = new Button(buttonComposite, SWT.NONE);
+		button.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		button.setText(buttonName);
+	}
+	
+	class FilterConditions{
+		private String fieldName;
+		private String relationalOperator;
+		private String conditionalOperator;
+		private String value;
+	}
 }
